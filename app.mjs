d7d5a4f78502d3b06e8c489b4f25f7dbf063fcac
Amiqa Saleem <amiqasaleem@gmail.com>
2025-03-22 02:21:48 +0500

    Changes

--- app.mjs
+++ app.mjs
@@ -1,11 +1,20 @@
 import express from "express"
 const port = 3000;
 const app = express();
+import cors from "cors";
+const router = express.Router();
+import routes from "./Routes/routes.js"
 
 app.get('/',(req, res)=>{
     res.send('Hello Worldddddd')
 })
+app.get('/users',(req, res)=>{
+    res.send('Hello Another got request is working')
+})
 
+app.post('/user', (req,res) => {
+    res.send('Got a POST Request')
+})
 
 app.listen(port,()=> {
     console.log(`Example app listening on port ${port}`);

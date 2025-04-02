62ea8c851f190335ac696545aa24d330b06d6490
Amiqa Saleem <amiqasaleem@gmail.com>
2025-04-02 12:54:09 +0500

    practice

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -8,92 +8,21 @@ let users = [];
 
 //for fetching the users
 
-const getAllUsers = (req, res) => {
-  console.log("arhe ha req");
-  try {
-    res.send(users);
-  } 
-  catch (e) {
-    console.log(e);
-  }
-};
 
 
 //for creating the user
 
-const createUser = async (req, res) => {
-  try {
-    const value = await userSchema.validateAsync(req.body);
-
-    let user = { ...req.body, id: nanoid() };
-    users.push(user);
-
-    res.status(201).json("user created successfully");
-    console.log(value);
-  } 
-  catch (error) {
-    res.status(500).json({
-      error: error?.details,
-    });
-  }
-};
 
 
 //for deleting user
 
-const deleteUser = (req, res) => {
-  try {
-    const { id } = req.params;
-
-    let index = users.findIndex((user) => user.id == id);
-    users.splice(index, 1);
-
-    res.send({
-      deletedId: id,
-      message: "user deleted successfully",
-    });
-  } 
-  catch (error) {
-    console.log(error);
-  }
-};
-
 
 //for Updating the user
 
-const updateUser = (req, res) => {
-  try {
-    const { id } = req.params;
-
-    let index = users.findIndex((user) => user.id == id);
-    users.splice(index, 1, { ...req.body, id: id });
-
-    res.send({
-      updatedUser: id,
-      message: "user updated successfully",
-    });
-  } 
-  catch (error) {
-    console.log(error);
-  }
-};
 
 //for fetching individual user
 
-const getUser = (req, res) => {
-  try {
-    const { id } = req.params;
-    let foundUser = users.find((obj) => obj.id == id);
 
-    res.send({
-      user: foundUser,
-      message: "user found successfully",
-    });
-  } 
-  catch (error) {
-    console.log(error);
-  }
-};
 
 export { getAllUsers, updateUser, deleteUser, getUser, createUser };
 
